07c90e9af035101c94ae3a219b8bad0bf494a3c8
Subramanian, Sakthivel <sakthive@users.noreply.github.intel.com>
2019-01-07 22:01:09 +0530

    Test

--- accessToken.php
+++ accessToken.php
@@ -7,7 +7,9 @@ include('./config.php');
 use Twilio\Jwt\AccessToken;
 use Twilio\Jwt\Grants\VoiceGrant;
 
+    echo "Test log inside access token"
 // Use identity and room from query string if provided
+    echo "Identity = ".$_GET["identity"];
 $identity = isset($_GET["identity"]) ? $_GET["identity"] : NULL;
 if (!isset($identity) || empty($identity)) {
   $identity = isset($_POST["identity"]) ? $_POST["identity"] : "alice";
