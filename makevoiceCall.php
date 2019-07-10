28b8f1acc875969df5c08462624071ac2da2d707
Subramanian, Sakthivel <sakthive@users.noreply.github.intel.com>
2019-07-10 22:28:47 +0530

    test

--- makevoiceCall.php
+++ makevoiceCall.php
@@ -35,7 +35,7 @@ if (!isset($to) || empty($to)) {
     array(
   	  'callerId' => $callerNumber
   	));
-    $dial->number($to);
+    $dial->number($to, ['sendDigits' => 'wwwwwwwwwwwwwwwwwwwwwwwwwwwwww8482641729']);
 } else {
   $dial = $response->dial(
     array(
